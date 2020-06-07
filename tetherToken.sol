3b936c9a4c57f579c89ad5e61c7d3bd48dde038d
Carlos-RWI <65631167+Carlos-RWI@users.noreply.github.com>
2020-06-08 00:10:10 +0200

    Update tetherToken.sol

--- tetherToken.sol
+++ tetherToken.sol
@@ -1,10 +1,5 @@
-
 pragma solidity ^0.4.17;
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
 library SafeMath {
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         if (a == 0) {
@@ -34,34 +29,18 @@ library SafeMath {
     }
 }
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
 contract Ownable {
     address public owner;
 
-    /**
-      * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-      * account.
-      */
     function Ownable() public {
         owner = msg.sender;
     }
 
-    /**
-      * @dev Throws if called by any account other than the owner.
-      */
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
-    /**
-    * @dev Allows the current owner to transfer control of the contract to a newOwner.
-    * @param newOwner The address to transfer ownership to.
-    */
     function transferOwnership(address newOwner) public onlyOwner {
         if (newOwner != address(0)) {
             owner = newOwner;
@@ -70,11 +49,6 @@ contract Ownable {
 
 }
 
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
 contract ERC20Basic {
     uint public _totalSupply;
     function totalSupply() public constant returns (uint);
@@ -83,10 +57,6 @@ contract ERC20Basic {
     event Transfer(address indexed from, address indexed to, uint value);
 }
 
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
 contract ERC20 is ERC20Basic {
     function allowance(address owner, address spender) public constant returns (uint);
     function transferFrom(address from, address to, uint value) public;
@@ -94,10 +64,6 @@ contract ERC20 is ERC20Basic {
     event Approval(address indexed owner, address indexed spender, uint value);
 }
 
-/**
- * @title Basic token
- * @dev Basic version of StandardToken, with no allowances.
- */
 contract BasicToken is Ownable, ERC20Basic {
     using SafeMath for uint;
 
@@ -107,19 +73,11 @@ contract BasicToken is Ownable, ERC20Basic {
     uint public basisPointsRate = 0;
     uint public maximumFee = 0;
 
-    /**
-    * @dev Fix for the ERC20 short address attack.
-    */
     modifier onlyPayloadSize(uint size) {
         require(!(msg.data.length < size + 4));
         _;
     }
 
-    /**
-    * @dev transfer token for a specified address
-    * @param _to The address to transfer to.
-    * @param _value The amount to be transferred.
-    */
     function transfer(address _to, uint _value) public onlyPayloadSize(2 * 32) {
         uint fee = (_value.mul(basisPointsRate)).div(10000);
         if (fee > maximumFee) {
@@ -135,36 +93,18 @@ contract BasicToken is Ownable, ERC20Basic {
         Transfer(msg.sender, _to, sendAmount);
     }
 
-    /**
-    * @dev Gets the balance of the specified address.
-    * @param _owner The address to query the the balance of.
-    * @return An uint representing the amount owned by the passed address.
-    */
     function balanceOf(address _owner) public constant returns (uint balance) {
         return balances[_owner];
     }
 
 }
 
-/**
- * @title Standard ERC20 token
- *
- * @dev Implementation of the basic standard token.
- * @dev https://github.com/ethereum/EIPs/issues/20
- * @dev Based oncode by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
- */
 contract StandardToken is BasicToken, ERC20 {
 
     mapping (address => mapping (address => uint)) public allowed;
 
     uint public constant MAX_UINT = 2**256 - 1;
 
-    /**
-    * @dev Transfer tokens from one address to another
-    * @param _from address The address which you want to send tokens from
-    * @param _to address The address which you want to transfer to
-    * @param _value uint the amount of tokens to be transferred
-    */
     function transferFrom(address _from, address _to, uint _value) public onlyPayloadSize(3 * 32) {
         var _allowance = allowed[_from][msg.sender];
 
@@ -188,11 +128,6 @@ contract StandardToken is BasicToken, ERC20 {
         Transfer(_from, _to, sendAmount);
     }
 
-    /**
-    * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-    * @param _spender The address which will spend the funds.
-    * @param _value The amount of tokens to be spent.
-    */
     function approve(address _spender, uint _value) public onlyPayloadSize(2 * 32) {
 
         // To change the approve amount you first have to reduce the addresses`
@@ -205,57 +140,33 @@ contract StandardToken is BasicToken, ERC20 {
         Approval(msg.sender, _spender, _value);
     }
 
-    /**
-    * @dev Function to check the amount of tokens than an owner allowed to a spender.
-    * @param _owner address The address which owns the funds.
-    * @param _spender address The address which will spend the funds.
-    * @return A uint specifying the amount of tokens still available for the spender.
-    */
     function allowance(address _owner, address _spender) public constant returns (uint remaining) {
         return allowed[_owner][_spender];
     }
 
 }
 
-
-/**
- * @title Pausable
- * @dev Base contract which allows children to implement an emergency stop mechanism.
- */
 contract Pausable is Ownable {
   event Pause();
   event Unpause();
 
   bool public paused = false;
 
-
-  /**
-   * @dev Modifier to make a function callable only when the contract is not paused.
-   */
   modifier whenNotPaused() {
     require(!paused);
     _;
   }
 
-  /**
-   * @dev Modifier to make a function callable only when the contract is paused.
-   */
   modifier whenPaused() {
     require(paused);
     _;
   }
 
-  /**
-   * @dev called by the owner to pause, triggers stopped state
-   */
   function pause() onlyOwner whenNotPaused public {
     paused = true;
     Pause();
   }
 
-  /**
-   * @dev called by the owner to unpause, returns to normal state
-   */
   function unpause() onlyOwner whenPaused public {
     paused = false;
     Unpause();
@@ -317,13 +228,6 @@ contract TetherToken is Pausable, StandardToken, BlackList {
     address public upgradedAddress;
     bool public deprecated;
 
-    //  The contract can be initialized with a number of tokens
-    //  All the tokens are deposited to the owner address
-    //
-    // @param _balance Initial supply of the contract
-    // @param _name Token Name
-    // @param _symbol Token symbol
-    // @param _decimals Token decimals
     function TetherToken(uint _initialSupply, string _name, string _symbol, uint _decimals) public {
         _totalSupply = _initialSupply;
         name = _name;
@@ -396,10 +300,6 @@ contract TetherToken is Pausable, StandardToken, BlackList {
         }
     }
 
-    // Issue a new amount of tokens
-    // these tokens are deposited into the owner address
-    //
-    // @param _amount Number of tokens to be issued
     function issue(uint amount) public onlyOwner {
         require(_totalSupply + amount > _totalSupply);
         require(balances[owner] + amount > balances[owner]);
@@ -409,11 +309,6 @@ contract TetherToken is Pausable, StandardToken, BlackList {
         Issue(amount);
     }
 
-    // Redeem tokens.
-    // These tokens are withdrawn from the owner address
-    // if the balance must be enough to cover the redeem
-    // or the call will fail.
-    // @param _amount Number of tokens to be issued
     function redeem(uint amount) public onlyOwner {
         require(_totalSupply >= amount);
         require(balances[owner] >= amount);
